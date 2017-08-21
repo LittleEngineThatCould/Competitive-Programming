a234861142a1b4d1ca1ac23e38ede04c6fb6fd60
LittleEngineThatCould <oasis1991rock@gmail.com>
2017-08-21 20:33:29 +0500

    Update

--- BigInteger.cpp
+++ BigInteger.cpp
@@ -1,12 +1,12 @@
 class BigInt
 {
 public:
-    vector<short> v;
+    vector<int> v;
     BigInt()
     {
         v.pb(0);
     }
-
+ 
     void output()
     {
         for(int i = v.size()-1; i >= 0; i--)
@@ -14,18 +14,19 @@ public:
             cout << v[i];
         }
     }
-
+ 
     BigInt operator+ (BigInt const &a)
     {
-        short carry = 0;
+        int carry = 0;
         int mx = max(a.v.size(), this->v.size());
-        vector<short> tmp(mx);
+        vector<int> tmp(mx);
         for(int i = 0; i < mx || carry; i++)
         {
             if(i > mx-1) tmp.pb(0);
             tmp[i] = (i > a.v.size()-1 ? 0: a.v[i]) + (i > this->v.size()-1 ? 0 : this->v[i]) + carry;
             carry = (int)(tmp[i] >= 10);
             if(carry) tmp[i] -= 10;
+            carry = (int)(tmp[i] >= 100000000);
         }
         BigInt ans;
         ans.v = tmp;
